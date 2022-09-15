53899a44cad3e3fd6d3269d04422902c248ea659
Erik-Jan van Kesteren <erikjanvankesteren@pm.me>
2022-09-15 09:23:31 +0200

    Simplify make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,26 +3,28 @@ using Documenter
 
 DocMeta.setdocmeta!(ProportionalFitting, :DocTestSetup, :(using ProportionalFitting); recursive=true)
 
-makedocs(;
-    modules=[ProportionalFitting],
-    authors="Erik-Jan van Kesteren",
-    repo="https://github.com/vankesteren/ProportionalFitting.jl/blob/{commit}{path}#{line}",
-    sitename="ProportionalFitting.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://vankesteren.github.io/ProportionalFitting.jl",
-        edit_link="main",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-        "Examples" => "examples.md",
-        "Benchmarks" => "benchmarks.md",
-        "Reference" => "reference.md"
-    ],
+frmt =  Documenter.HTML(;
+    prettyurls = get(ENV, "CI", "false") == "true",
+    canonical = "https://vankesteren.github.io/ProportionalFitting.jl",
+    edit_link = "main",
+    assets = String[],
 )
 
-deploydocs(;
-    repo="github.com/vankesteren/ProportionalFitting.jl",
-    versions = ["stable" => "v^", "v#.#", "dev" => "main"],
+pgs = [
+    "Home" => "index.md",
+    "Examples" => "examples.md",
+    "Benchmarks" => "benchmarks.md",
+    "Reference" => "reference.md"
+]
+
+makedocs(;
+    modules = [ProportionalFitting],
+    authors = "Erik-Jan van Kesteren",
+    repo = "https://github.com/vankesteren/ProportionalFitting.jl",
+    devbranch = "main",
+    sitename = "ProportionalFitting.jl",
+    format = frmt,
+    pages = pgs,
 )
+
+deploydocs(; repo = "github.com/vankesteren/ProportionalFitting.jl")
