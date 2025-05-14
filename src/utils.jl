1c7f22f5201476475ba9e1de294d773fba71a08f
James Alster <jamesalster2@gmail.com>
2025-05-14 16:15:15 +0100

    remove dict methods

--- src/utils.jl
+++ src/utils.jl
@@ -1,6 +1,3 @@
-# dict methods for future reference
-Base.Dict(AM::ArrayMargins) = Dict(AM.di[i] => AM.am[i] for i in 1:length(AM))
-Base.Dict(AF::ArrayFactors) = Dict(AF.di[i] => AF.af[i] for i in 1:length(AF))
 
 # method to align margins according to the size of a larger array
 function align_margins(arr::AbstractArray, idx::Vector{Int}, shape::Tuple)
